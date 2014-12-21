fcd1ae8c21e8423b996b8a7e5c8ed8640ae63e5b
xbony2 <xbony22@gmail.com>
2014-12-20 21:08:13 -0500

    Messing around

--- src/main/ruby/xbony2/ircbot/bonybot.rb
+++ src/main/ruby/xbony2/ircbot/bonybot.rb
@@ -1,16 +1,31 @@
 require 'cinch'
 
-botName = "ESAEBSAD"
+BOT_NAME = "ESAEBSAD"
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.esper.net"
     c.channels = ["#NuclearControl2"]
+    c.nick = BOT_NAME
   end
 
-  on :message, "hello" do |m|
-    m.reply "Hello, #{m.user.nick}"
-  end
+    # Only log channel messages
+    on :channel do |m|
+      $users[m.user.nick] = Seen.new(m.user.nick, m.channel, m.message, Time.new)
+    end
+  
+    on :channel, /^!seen (.+)/ do |m, nick|
+      if nick == bot.nick
+        m.reply "That's me!"
+      elsif nick == m.user.nick
+        m.reply "That's you!"
+      elsif $users.key?(nick)
+        m.reply $users[nick].to_s
+        m.reply "test"
+      else
+        m.reply "I haven't seen #{nick}"
+      end
+    end
 end
 
 bot.start
