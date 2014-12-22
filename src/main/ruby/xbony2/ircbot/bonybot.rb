a9af260390cbb986c25e2a9b3cbe759cbb401a7d
xbony2 <xbony22@gmail.com>
2014-12-21 20:09:02 -0500

    Update

--- src/main/ruby/xbony2/ircbot/bonybot.rb
+++ src/main/ruby/xbony2/ircbot/bonybot.rb
@@ -11,8 +11,9 @@ bot = Cinch::Bot.new do
   
   on :channel, /^@@help/ do |m|
     m.reply "Commands:"
-    m.reply ": @@help -displays this message"
-    m.reply ": @@flip -flips a coin"
+    m.reply ": @@help -displays this message,"
+    m.reply ": @@flip -flips a coin,"
+    m.reply ": @@roll -rolls a die."
   end
   
   on :channel, /^@@flip/ do |m|
@@ -21,6 +22,25 @@ bot = Cinch::Bot.new do
       m.reply "The coin flip reveals heads."
     else
       m.reply "The coin flip reveals tails."
+    end
+  end
+  
+  on :channel, /^@@roll/ do |m|
+    result = 1 + rand(6)
+    m.reply "The die roll reveals the number #{result}."
+  end
+  
+  on :channel, /^@@stop/ do |m|
+    if m.user.authname == "xbony2"
+      exit
+    else
+      m.reply "You cannot stop me unless you are my owner."
+    end 
+  end
+  
+  on :leaving do |m, user|
+    if user.nick == bot.nick
+      exit
     end
   end
 end
