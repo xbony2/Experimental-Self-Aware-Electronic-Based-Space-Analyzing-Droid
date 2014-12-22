7ac300515783ce42f84c1d443e4fb9fbac1e4746
xbony2 <xbony22@gmail.com>
2014-12-21 19:22:19 -0500

    Actually usefulish things

--- src/main/ruby/xbony2/ircbot/bonybot.rb
+++ src/main/ruby/xbony2/ircbot/bonybot.rb
@@ -9,8 +9,19 @@ bot = Cinch::Bot.new do
     c.nick = BOT_NAME
   end
   
-  on :channel, /^@@sayHi/ do |m|
-    m.reply "Hello!"
+  on :channel, /^@@help/ do |m|
+    m.reply "Commands:"
+    m.reply ": @@help -displays this message"
+    m.reply ": @@flip -flips a coin"
+  end
+  
+  on :channel, /^@@flip/ do |m|
+    result = 1 + rand(2)
+    if result == 1
+      m.reply "The coin flip reveals heads."
+    else
+      m.reply "The coin flip reveals tails."
+    end
   end
 end
 
