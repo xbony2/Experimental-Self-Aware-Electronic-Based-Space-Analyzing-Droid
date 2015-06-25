49b72e32da36a45b4ca70aa9babbdbe9186b4914
xbony2 <xbony22@gmail.com>
2015-06-25 17:58:11 -0400

    smarter bot

--- src/main/ruby/xbony2/ircbot/plugins/lyrics.rb
+++ src/main/ruby/xbony2/ircbot/plugins/lyrics.rb
@@ -13,8 +13,12 @@ class Lyrics
     if msg.user.authname != $OWNER_NAME #restricted because it's basically a spam machine
       msg.reply "You are not authorized. Ask #{$OWNER_NAME} for any requests."
     else
-      lyrics = @@lyric_getter.search(artist, song).body.split("\\n")
-      lyrics.each {|str| msg.reply(str)}
+      begin
+        lyrics = @@lyric_getter.search(artist, song).body.split("\\n")
+        lyrics.each {|str| msg.reply(str)}
+      rescue NoMethodError
+        msg.reply "Song not found! Or it may be broken. Remember, artist; song."
+      end
     end
   end
 end
