3c5e0ab794e2684f16684b6f3719b9f1b733c523
xbony2 <xbony22@gmail.com>
2015-08-19 14:57:53 -0400

    herpa derpa

--- src/main/ruby/xbony2/ircbot/plugins/trans.rb
+++ src/main/ruby/xbony2/ircbot/plugins/trans.rb
@@ -13,24 +13,20 @@ class Trans
       end
           
       text = JSON.parse($other_wiki_bot.get_wikitext(page))["query"]["pages"][$revid]["revisions"][0]["*"]
-      text = text.gsub(/\[\[Category:.+\]\]/){|s| s.gsub /\]\]/, "{{L}}]]"} #Does categories
+      text = text.gsub(/\[\[Category:.+\]\]/){|s| s.gsub /\]\]/, "{{L}}]]"}
       text = text.gsub(/<br\/>/, "<br />")
       text = text.gsub(/<languages\/>/, "<languages />")
-      text = text.gsub(/\[\[.+\]\]/){|s| (!s.start_with?("[[Category:") and !s.start_with?("[[File:")) ? s.gsub(/\[\[/, "{{L|").gsub(/\]\]/, "}}") : s} #Does links
-      text = text.gsub(/\{\{[Ii]nfobox\n/, "{{Infobox{{L}}\n") #Does infobox
-      text = text.gsub(/\{\{[Ii]nfobox mod\n/, "{{Infobox mod{{L}}\n") #Does infobox mod
-      ["name", "lore", "module", "effects", "storageslots", "storage",  "exp", "modpacks", "requires", "dependency", "neededfor", "neededforpast", "requirespast",  "dependecypast", 
-        "description"].each {|s| #Does parameters
+      text = text.gsub(/\[\[.+\]\]/){|s| (!s.start_with?("[[Category:") and !s.start_with?("[[File:")) ? s.gsub(/\[\[/, "{{L|").gsub(/\]\]/, "}}") : s}
+      text = text.gsub(/\{\{[Ii]nfobox\n/, "{{Infobox{{L}}\n")
+      text = text.gsub(/\{\{[Ii]nfobox mod\n/, "{{Infobox mod{{L}}\n")
+      ["name", "lore", "module", "effects", "storageslots", "storage", "exp", "modpacks", "requires", "dependency", "neededfor", "neededforpast", "requirespast",  "dependecypast", "description"].each {|s|
         text = text.gsub(/\|#{s}=.+\n/){|ns| ns.insert(2 + s.length, "<translate>").insert(-2, "</translate>")}
       }
           
-      #text = text.gsub(/\|mod=.+\n/){|s| !s.end_with?("}}\n") ? s.gsub(/|mod=.+\n/){|ns| ns.insert(5, "<translate>").insert(-2, "</translate>")} : s}
-      #No idea why, but that doesn't work ^
-      text = text.gsub(/\{\{Cg\/.+\n/){|s| s.insert -2, "{{L}}"} # Does crafting grids
-      text = text.gsub(/\{\{Navbox .+\}\}/){|s| s.insert -3, "{{L}}"} #Does navboxes
-      text = text.insert(0, "<translate><!--Translators note: you don't needed to translate this line. Just copy-paste it over. Anyway, this page was originally translated before " + 
-        "the module was put in place, using whatever older system there was. I made backups of previously translated pages, so you can use them for reference. Checkout: " + 
-        "[[User:Xbony2#My_subpages]]--></translate>\n") if special == "in"
+      text = text.gsub(/\|mod=.+\n/){|s| !s.end_with?("}}\n") ? s.insert(5, "<translate>").insert(-2, "</translate>") : s}
+      text = text.gsub(/\{\{Cg\/.+\n/){|s| s.insert -2, "{{L}}"}
+      text = text.gsub(/\{\{Navbox .+\}\}/){|s| s.insert -3, "{{L}}"}
+      text = text.insert(0, "<!--Translators note: this article is part of the [[Feed The Beast Wiki:Translation Restoration project|Translation Restoration project]]-->\n") if special == "in"
       $wiki_bot.edit(title: page, text: text, bot: 1, summary: "Add translation markup.")
       msg.reply "Here you go: http://ftb.gamepedia.com/#{page.gsub(' ', '_')}"
     end
