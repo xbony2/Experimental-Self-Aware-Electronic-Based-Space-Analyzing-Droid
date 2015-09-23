3dd6089076469c58491516db04d5e5b338cbafa2
xbony2 <xbony22@gmail.com>
2015-09-23 17:10:51 -0400

    fixed dumb shit

--- src/main/ruby/xbony2/ircbot/plugins/addcata.rb
+++ src/main/ruby/xbony2/ircbot/plugins/addcata.rb
@@ -4,14 +4,14 @@ class Addcata
   set :prefix, /^@@/
   match /addcata (.*); (.*); (.*)/
   def execute(msg, sub1, sub2, name)
-    if m.user.authname != $OWNER_NAME
-      m.reply "You are not authorized."
+    if msg.user.authname != $OWNER_NAME
+      msg.reply "You are not authorized."
       return
     elsif sub2 != "nil"
       $wiki_bot.edit(title: "Category:#{name}", text: "[[Category:#{sub1}]]\n[[Category:#{sub2}]]", bot: 1)
     else
-        $wiki_bot.edit(title: "Category:#{name}", text: "[[Category:#{sub1}]]", bot: 1)
+      $wiki_bot.edit(title: "Category:#{name}", text: "[[Category:#{sub1}]]", bot: 1)
     end
-    m.reply "Here you go: http://ftb.gamepedia.com/Category:#{name.gsub(' ', '_')}"
+    msg.reply "Here you go: http://ftb.gamepedia.com/Category:#{name.gsub(' ', '_')}"
   end
 end
