88c6635101873a516567ae83dd912ff88319014c
xbony2 <xbony22@gmail.com>
2015-10-02 15:44:06 -0400

    experimental command

--- src/main/ruby/xbony2/ircbot/bonybot.rb
+++ src/main/ruby/xbony2/ircbot/bonybot.rb
@@ -21,6 +21,7 @@ require_relative 'plugins/quote'
 require_relative 'plugins/motivate'
 require_relative 'plugins/flirt'
 require_relative 'plugins/say_stuff'
+require_relative 'plugins/catmanipulator'
 
 $BOT_NAME = 'ESAEBSAD'
 $OWNER_NAME = 'xbony2'
@@ -30,7 +31,7 @@ bot = Cinch::Bot.new do
     c.server = "irc.esper.net"
     c.channels = ["#NuclearControl2", "#FTB-Wiki"]
     c.nick = $BOT_NAME
-    c.plugins.plugins = [Help, Help_Advanced, Trans, Addcat, Addriovarmor, Addcata, Lyrics, Quote, Motivate, Flirt, Say_Stuff]
+    c.plugins.plugins = [Help, Help_Advanced, Trans, Addcat, Addriovarmor, Addcata, Lyrics, Quote, Motivate, Flirt, Say_Stuff, Catmanipulator]
     
     $butt = MediaWiki::Butt.new 'http://ftb.gamepedia.com'
     $butt.login($BOT_NAME, File.read('git/IRC-Bot/src/main/resources/xbony2/ircbot/SEKRET_PASSWORD.confidentual'))
---
+++ src/main/ruby/xbony2/ircbot/plugins/catmanipulator.rb
@@ -0,0 +1,16 @@
+class Catmanipulator
+  include Cinch::Plugin
+  
+  set :prefix, /^@@/
+  match /catmanipulate (.*); (.*)/
+  def execute(msg, cat, newcat)
+    if msg.user.authname != $OWNER_NAME
+      msg.reply "You are not authorized."
+    else
+      newcat = newcat == "nil" ? "" : "[[Category:#{newcat}]]"
+      $butt.get_category_members("Category:#{cat}").each {|page| 
+        $butt.edit(page, $butt.get_text(page).gsub(/\[\[Category:#{cat}\]\]/, newcat), "Modified category.")
+      }
+    end
+  end
+end
