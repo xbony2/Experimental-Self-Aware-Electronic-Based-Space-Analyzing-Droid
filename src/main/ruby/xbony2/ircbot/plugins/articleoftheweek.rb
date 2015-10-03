43a139bac25bcd4f1843227824ccaba072f5fb7f
xbony2 <xbony22@gmail.com>
2015-10-03 09:52:32 -0400

    spelling :P

--- src/main/ruby/xbony2/ircbot/plugins/articleoftheweek.rb
+++ src/main/ruby/xbony2/ircbot/plugins/articleoftheweek.rb
@@ -2,7 +2,7 @@ class Article_Of_The_Week
   include Cinch::Plugin
   
   set :prefix, /^@@/
-  match "articleofthweek"
+  match "articleoftheweek"
   def execute(msg)
     msg.reply "The article of the week is..."
     page = $butt.get_random_pages[0]
