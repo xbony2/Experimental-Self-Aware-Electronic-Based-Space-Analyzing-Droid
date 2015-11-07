6299c9920bed9c537e2ee8082048bfbde5856c95
xbony2 <xbony22@gmail.com>
2015-11-06 19:43:19 -0500

    silly optimisation

--- src/main/ruby/xbony2/ircbot/plugins/replaceallincategory.rb
+++ src/main/ruby/xbony2/ircbot/plugins/replaceallincategory.rb
@@ -7,8 +7,9 @@ class ReplaceAllInCategory
     if msg.user.authname != $OWNER_NAME
       msg.reply "You are not authorized."
     else
-      $ftb_client.get_category_members("Category:#{cat}").each do |page| 
-        $ftb_client.edit(page, $ftb_client.get_text(page).gsub(/#{oldtext}/, newtext), summary: "Converting \"#{oldtext}\" to \"#{newtext}\"")
+      $ftb_client.get_category_members("Category:#{cat}").each do |page|
+        text = $ftb_client.get_text(page)
+        $ftb_client.edit(page, text.gsub(/#{oldtext}/, newtext), summary: "Converting \"#{oldtext}\" to \"#{newtext}\"") if text.include?(oldtext)
       end
       msg.reply "Process complete."
     end
