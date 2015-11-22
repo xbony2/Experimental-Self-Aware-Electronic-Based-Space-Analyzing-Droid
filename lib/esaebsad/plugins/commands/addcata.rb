f4625431ad14c7f73e00796420830e3f00bd27f2
xbony2 <xbony22@gmail.com>
2015-11-22 12:35:44 -0500

    refactor pt11

--- lib/esaebsad/plugins/commands/addcata.rb
+++ lib/esaebsad/plugins/commands/addcata.rb
@@ -2,25 +2,31 @@ class AddCatA < ESAEBSADCommand
   include Cinch::Plugin
   
   set_help "addcata", <<EOS
-Group: owner. Syntax: "@@addcata (name); (sub1); [sub2]"
-The addcata command will create a new category based on one/two subcategories.
+Group: owner. Syntax: "@@addcata (name); (sub1); [sub2]; [sub3]"
+The addcata command will create a new category based on 1-3 subcategories.
 EOS
   set :prefix, /^@@/
   match /addcata (.*); (.*)/, method: :one_sub
   match /addcata (.*); (.*); (.*)/, method: :two_sub
+  match /addcata (.*); (.*); (.*); (.*)/, method: :three_sub
   
   def one_sub(msg, name, sub)
-    if is_part_of_group? msg.user.authname, :owner
-      get_client.create_page("Category:#{name}", "[[Category:#{sub1}]]", "Created category page.")
-      msg.reply "You are not authorized."
-    else
-      msg.reply "You are not authorized."
-    end
+    create_categories(msg, name, [sub])
   end
   
   def two_sub(msg, name, sub1, sub2)
+    create_categories(msg, name, [sub1, sub2])
+  end
+  
+  def three_sub(msg, name, sub1, sub2, sub3)
+    create_categories(msg, name, [sub1, sub2, sub3])
+  end
+  
+  def create_categories(msg, name, sub = [])
     if is_part_of_group? msg.user.authname, :owner
-      get_client.create_page("Category:#{name}", "[[Category:#{sub1}]]\n[[Category:#{sub2}]]", "Created category page.")
+      text = ""
+      sub.each {|s| text << "[[Category:#{s}]]\n"}
+      get_client.create_page("Category:#{name}", text, "Created category page.")
       msg.reply "Here you go: http://ftb.gamepedia.com/Category:#{urlize(name)}"
     else
       msg.reply "You are not authorized."
