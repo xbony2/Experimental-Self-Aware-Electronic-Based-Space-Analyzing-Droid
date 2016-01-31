a9f407c978c0a25f60f3c4b9e4d3cca1e320dc95
xbony2 <xbony22@gmail.com>
2016-01-31 09:28:05 -0500

    Changed addcat command to addcatt (add category template) and renamed addcata to addcat

--- lib/esaebsad/plugins/commands/addcat.rb
+++ lib/esaebsad/plugins/commands/addcat.rb
@@ -1,25 +1,34 @@
-class AddCat < ESAEBSADCommand
+class AddCatA < ESAEBSADCommand
   include Cinch::Plugin
   include ESAEBSAD::Utility
   extend ESAEBSAD::Utility
-  
+
   create_help "addcat", <<EOS
-Group: ftbop. Syntax: "@@addcat (type); (name)"
-The addcat command will create a new category based on the type.
-Example: "@@addcat mod; Thermal Expansion 3".
+Group: ftbop. Syntax: "@@addcat (name); (sub1); [sub2]; [sub3]"
+The addcat command will create a new category based on 1-3 subcategories.
 EOS
   set :prefix, /^@@/
-  match /addcat (.+); (.*)/
-  def execute(msg, type, name)
-    if is_part_of_group? msg.user.authname, "ftbop"
-      case type
-      when "mod" then get_client.create_page("Category:#{name}", "[[Category:Mod categories]]\n[[Category:Mods]]", "Created category page.")
-      when "minor" then get_client.create_page("Category:#{name}", "[[Category:Mod categories]]\n[[Category:Minor Mods]]", "Created category page.")
-      else
-        msg.reply "You screwed up. Try again."
-        return
-      end
+  match /addcat (.*); (.*)/, method: :one_sub
+  match /addcat (.*); (.*); (.*)/, method: :two_sub
+  match /addcat (.*); (.*); (.*); (.*)/, method: :three_sub
 
+  def one_sub(msg, name, sub)
+    create_categories(msg, name, [sub])
+  end
+
+  def two_sub(msg, name, sub1, sub2)
+    create_categories(msg, name, [sub1, sub2])
+  end
+
+  def three_sub(msg, name, sub1, sub2, sub3)
+    create_categories(msg, name, [sub1, sub2, sub3])
+  end
+
+  def create_categories(msg, name, sub = [])
+    if is_part_of_group? msg.user.authname, "ftbop"
+      text = ""
+      sub.each {|s| text << "[[Category:#{s}]]\n"}
+      get_client.create_page("Category:#{name}", text, "Created category page.")
       msg.reply "Here you go: http://ftb.gamepedia.com/Category:#{urlize(name)}"
     else
       msg.reply "You are not authorized."
---
+++ lib/esaebsad/plugins/commands/addcatt.rb
@@ -0,0 +1,28 @@
+class AddCat < ESAEBSADCommand
+  include Cinch::Plugin
+  include ESAEBSAD::Utility
+  extend ESAEBSAD::Utility
+  
+  create_help "addcatt", <<EOS
+Group: ftbop. Syntax: "@@addcatt (type); (name)"
+The addcatt command will create a new category based on the type.
+Example: "@@addcatt mod; Thermal Expansion 3".
+EOS
+  set :prefix, /^@@/
+  match /addcatt (.+); (.*)/
+  def execute(msg, type, name)
+    if is_part_of_group? msg.user.authname, "ftbop"
+      case type
+      when "mod" then get_client.create_page("Category:#{name}", "[[Category:Mod categories]]\n[[Category:Mods]]", "Created category page.")
+      when "minor" then get_client.create_page("Category:#{name}", "[[Category:Mod categories]]\n[[Category:Minor Mods]]", "Created category page.")
+      else
+        msg.reply "You screwed up. Try again."
+        return
+      end
+
+      msg.reply "Here you go: http://ftb.gamepedia.com/Category:#{urlize(name)}"
+    else
+      msg.reply "You are not authorized."
+    end
+  end
+end
