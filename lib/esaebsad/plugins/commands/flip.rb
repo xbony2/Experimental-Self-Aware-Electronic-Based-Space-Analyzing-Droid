9f201efdf292744a364d19cae57508722c1d7fed
xbony2 <xbony22@gmail.com>
2016-01-31 19:42:58 -0500

    Closes #10 (that was embarssing)

--- lib/esaebsad/plugins/commands/flip.rb
+++ lib/esaebsad/plugins/commands/flip.rb
@@ -7,7 +7,6 @@ class Flip < ESAEBSADCommand
   set :prefix, /^@@/
   match "flip"
   def execute(msg)
-    msg.reply "The coin flip reveals #{rand(2) == 0 ? "heads" : "tails"}."
     msg.reply(localize("command.flip.result", rand(2) == 0 ? localize("command.flip.heads") : localize("command.flip.tails")))
   end
 end
