1356e2fd1836bf65a7ced1e1fd1cf4b2a8d87a89
Eric Schneider <xbony22@gmail.com>
2017-06-21 12:00:17 -0400

    Updated to MWB 2. The info command can now take no parameters and uses the commander. also names auto-capitalized

--- lib/esaebsad/plugins/commands/info.rb
+++ lib/esaebsad/plugins/commands/info.rb
@@ -3,13 +3,18 @@ class Info < ESAEBSADCommand
   include ESAEBSAD::Utility
   extend ESAEBSAD::Utility
 
-  match /info (.*)/, method: :default
+  match /info/, method: :default1
+  match /info (.*)/, method: :default2
   match /info (.+); (.*)/, method: :execute
-  
-  def default(msg, user)
+
+  def default1(msg)
+    execute(msg, WIKI_CORE, msg.user.nick)
+  end
+
+  def default2(msg, user)
     execute(msg, WIKI_CORE, user)
   end
-  
+
   def execute(msg, wiki, user)
     client = nil
     if wiki.start_with?("http://", "https://")
@@ -17,11 +22,15 @@ class Info < ESAEBSADCommand
     else
       client = get_client(wiki)
     end
-    
-    msg.reply(localize("command.info", user, commafy(client.get_contrib_count(user)), client.get_registration_time(user).strftime("%B %e, %Y"), client.get_user_gender(user)))
+
+    msg.reply(localize("command.info", cap(user), commafy(client.get_contrib_count(cap(user))), client.get_registration_time(cap(user)).strftime("%B %e, %Y"), client.get_user_gender(cap(user))))
   end
-  
+
   def commafy(int)
     int.to_s.chars.reverse!.each_slice(3).map(&:join).join(",").reverse!
+  end
+
+  def cap(str)
+    str[0].capitalize + str[1..str.length]
   end
 end
